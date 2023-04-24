d654599a5effc24f8711e09a73ad2723adb5c163
Ryan Phan <ryanwithcodes@gmail.com>
2023-04-24 16:27:57 +0700

    ref: :sparkles: - add access failure handler in configuration

--- src/main/java/com/example/oauthstarter/infrastructure/security/AuthAccessFailureHandler.java
+++ src/main/java/com/example/oauthstarter/infrastructure/security/AuthAccessFailureHandler.java
@@ -1,6 +1,5 @@
 package com.example.oauthstarter.infrastructure.security;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -9,8 +8,6 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerExceptionResolver;
-
-import java.io.IOException;
 
 @Slf4j
 @Component
